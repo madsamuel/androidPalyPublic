c3dce4a30f767a8ac157a507d0fe52689eb67d31
madsamuel <madsamuel@gmail.com>
2023-06-16 22:54:59 -0700

    Sample UI adding a load bar

--- layouts and UI/splashScreenV3/app/src/main/java/com/seattledevcamp/splashscreenv3/SplashScreen.kt
+++ layouts and UI/splashScreenV3/app/src/main/java/com/seattledevcamp/splashscreenv3/SplashScreen.kt
@@ -4,17 +4,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ProgressBar;
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000) // in milliseconds.
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.max = 100
+        progressBar.progress = 0
+
+        Thread {
+            // Increment the progress bar for each step in the loading process
+            for (i in 0 until 100) {
+                Thread.sleep(10)
+                progressBar.progress = i + 1
+            }
+
+            // Stop the loading process and hide the progress bar
+            runOnUiThread {
+                progressBar.visibility = View.GONE
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 1000) // in milliseconds.
     }
 }
